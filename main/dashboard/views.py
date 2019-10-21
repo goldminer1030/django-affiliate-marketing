ec2d5592dcd7cc783710271db31335d8f6df2c07
goldminer <goldminer1030@gmail.com>
2019-10-22 05:40:03 +0800

    Updated the affiliates page

--- main/dashboard/views.py
+++ main/dashboard/views.py
@@ -12,7 +12,7 @@ class DashboardView(LoginRequiredMixin, ListView):
     context_object_name = 'users'
 
     def get_queryset(self):
-        return User.objects.all()
+        return User.objects.exclude(email=self.request.user.email)
 
 
 class OffersView(LoginRequiredMixin, CreateView):
@@ -28,66 +28,32 @@ class OffersView(LoginRequiredMixin, CreateView):
         return context
 
 
-class AffiliatesView(LoginRequiredMixin, CreateView):
+class AffiliatesView(LoginRequiredMixin, ListView):
     success_url = '/dashboard/affiliates/'
     template_name = 'dashboard/affiliates.html'
+    model = User
+    context_object_name = 'users'
+    paginate_by = 10
 
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, self.template_name, context)
-
-    def get_context_data(self, **kwargs):
-        context = super(AffiliatesView, self).get_context_data(**kwargs)
-        return context
+    def get_queryset(self):
+        return User.objects.exclude(email=self.request.user.email)
 
 
 class FinancesView(LoginRequiredMixin, CreateView):
     success_url = '/dashboard/finances/'
     template_name = 'dashboard/finances.html'
 
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, self.template_name, context)
-
-    def get_context_data(self, **kwargs):
-        context = super(FinancesView, self).get_context_data(**kwargs)
-        return context
-
 
 class MailRoomView(LoginRequiredMixin, CreateView):
     success_url = '/dashboard/mail-room/'
     template_name = 'dashboard/mail-room.html'
-
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, self.template_name, context)
-
-    def get_context_data(self, **kwargs):
-        context = super(MailRoomView, self).get_context_data(**kwargs)
-        return context
 
 
 class ToolsView(LoginRequiredMixin, CreateView):
     success_url = '/dashboard/tools/'
     template_name = 'dashboard/tools.html'
 
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, self.template_name, context)
-
-    def get_context_data(self, **kwargs):
-        context = super(ToolsView, self).get_context_data(**kwargs)
-        return context
-
 
 class AdministrationView(LoginRequiredMixin, CreateView):
     success_url = '/dashboard/administration/'
     template_name = 'dashboard/administration.html'
-
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request, self.template_name, context)
-
-    def get_context_data(self, **kwargs):
-        context = super(AdministrationView, self).get_context_data(**kwargs)
-        return context
