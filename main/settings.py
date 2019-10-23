60f6e34fd635c7d6db5c8edc95010dbd11481d21
goldminer <goldminer1030@gmail.com>
2019-10-24 01:31:36 +0800

    Added config for heroku

--- main/settings.py
+++ main/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -140,3 +141,5 @@ AUTH_USER_MODEL = 'profiles.User'
 LOGIN_URL = 'profiles:auth_login'
 LOGIN_REDIRECT_URL = 'dashboard:dashboard'
 LOGOUT_REDIRECT_URL = LOGIN_URL
+
+django_heroku.settings(locals())
